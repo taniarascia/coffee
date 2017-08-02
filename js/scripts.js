e657f14a6516d5cbd469c6a8127c43052814c41d
Tania Rascia <taniarascia@gmail.com>
2017-08-01 22:15:22 -0500

    Make sure

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ const coffeeCup = L.icon({
 });
 
 // Define basemap
-var myBasemap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+var myBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
 	maxZoom: 16
 });
